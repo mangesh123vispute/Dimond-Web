e1772745ae7ec2197ef2f8f38ce85285cc3f3ccf
mangesh vispute <mangesh2003vispute@gmail.com>
2024-07-15 16:19:06 +0530

    updated code

---
+++ Frontend/Dimond/src/App.jsx
@@ -0,0 +1,92 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/no-unknown-property */
+import "./App.css";
+import * as THREE from "three";
+
+import { Canvas, useLoader } from "@react-three/fiber";
+import {
+  useGLTF,
+  MeshRefractionMaterial,
+  AccumulativeShadows,
+  RandomizedLight,
+  Environment,
+  Center,
+  PresentationControls,
+  OrbitControls,
+} from "@react-three/drei";
+import { EffectComposer, Bloom } from "@react-three/postprocessing";
+import { RGBELoader } from "three-stdlib";
+
+function Ring({ map, ...props }) {
+  const { nodes, materials } = useGLTF("/model8.glb");
+  console.log("This is the nodes", nodes);
+
+  console.log("This is the materials", materials);
+
+  return (
+    <group {...props} dispose={null}>
+      {Object.keys(nodes)
+        .filter((key) => key.startsWith("Diamond_Round"))
+        .map((key, index) => (
+          <mesh key={key} geometry={nodes[key].geometry}>
+            <MeshRefractionMaterial
+              envMap={map}
+              aberrationStrength={0.02}
+              toneMapped={false}
+            />
+          </mesh>
+        ))}
+    </group>
+  );
+}
+
+export default function App() {
+  const texture = useLoader(
+    RGBELoader,
+    "https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/1k/peppermint_powerplant_2_1k.hdr"
+  );
+  texture.mapping = THREE.EquirectangularReflectionMapping;
+  return (
+    <Canvas shadows camera={{ position: [20, 20, 20], fov: 75 }} dpr={[1, 2]}>
+      <OrbitControls />
+      <axesHelper position={[0, 0, 0]} scale={5} />
+      <color attach="background" args={["#f0f0f0"]} />
+      <ambientLight />
+      <Environment map={texture} />
+      <PresentationControls
+        global
+        config={{ mass: 1, tension: 250, friction: 25 }}
+        snap={{ mass: 2, tension: 250, friction: 50 }}
+        zoom={1.25}
+        rotation={[0.5, 0.5, 0]}
+        polar={[-Math.PI / 5, Math.PI / 4]}
+        azimuth={[-Math.PI / 1.75, Math.PI / 4]}
+      >
+        <group position={[0, -3, 0]}>
+          <Center position={[0, 0, 0]}>
+            <Ring map={texture} scale={0.5} />
+          </Center>
+          <AccumulativeShadows
+            temporal
+            frames={100}
+            alphaTest={0.95}
+            opacity={1}
+            scale={20}
+          >
+            <RandomizedLight
+              amount={8}
+              radius={10}
+              ambient={0.5}
+              position={[0, 10, -2.5]}
+              bias={0.001}
+              size={3}
+            />
+          </AccumulativeShadows>
+        </group>
+      </PresentationControls>
+      <EffectComposer>
+        <Bloom luminanceThreshold={1} intensity={0.85} levels={9} mipmapBlur />
+      </EffectComposer>
+    </Canvas>
+  );
+}
